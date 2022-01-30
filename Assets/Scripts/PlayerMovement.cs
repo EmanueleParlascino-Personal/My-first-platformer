9f890cef8ecdf64aed482a2dfc744c49b4b8b02b
Emanuele Parlascino <emapage95@gmail.com>
2022-01-30 11:45:50 +0000

    added player movement and jump functionality

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,21 +7,56 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     Vector2 moveInput;
-   
+    Rigidbody2D myRigidbody;
+    [SerializeField] float playerSpeed = 3f;
+    [SerializeField] float jumpForce = 5f;
+    Animator myAnimator;
+    Collider2D myCollider;
     void Start()
     {
-        
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+        myCollider = GetComponent<Collider2D>();
     }
 
     void Update()
     {
-        
+        Run();
+        FlipSprite();
     }
+
+    void FlipSprite()
+    {
+        bool isMoving = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+        if(isMoving)
+        {
+            transform.localScale = new Vector2 (Mathf.Sign(myRigidbody.velocity.x), 1);
+        }
+    }
+
+    
 
     void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
         Debug.Log(moveInput);
+    }
+
+    void Run()
+    {
+        Vector2 playerVelocity = new Vector2(moveInput.x * playerSpeed, myRigidbody.velocity.y);
+        myRigidbody.velocity = playerVelocity;
+
+        bool isMoving = Mathf.Abs(myRigidbody.velocity.x) > Mathf.Epsilon;
+        myAnimator.SetBool("isRunning", isMoving);
+    }
+
+    void OnJump(InputValue value)
+    {
+        if(value.isPressed && myCollider.IsTouchingLayers(LayerMask.GetMask("Ground")))
+        {
+            myRigidbody.velocity += new Vector2(0, jumpForce);
+        }
     }
 
     
